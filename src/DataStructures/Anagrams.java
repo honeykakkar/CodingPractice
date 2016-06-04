d75a7b4b5100e53c8b914ccd9c5570e5511af880
Honey Kakkar <honeykakkar09@gmail.com>
2016-06-04 01:11:17 -0400

    Adding basic problems related to array data structure

---
+++ src/DataStructures/Anagrams.java
@@ -0,0 +1,44 @@
+package DataStructures;
+import java.util.Scanner;
+
+/**
+ * Created by honey on 6/4/2016.
+ */
+
+// Given two strings, write a method to decide if one is a permutation of the other.
+
+public class Anagrams
+{
+    boolean isAnagram(CharSequence input1, CharSequence input2)
+    {
+        if(input1.length() != input2.length())
+            return false;
+        int[] char_flags = new int[256];
+        for(int i=0; i<input1.length(); ++i)
+            ++char_flags[input1.charAt(i)];
+
+        for (int i=0; i<input2.length(); ++i)
+        {
+            if(--char_flags[input2.charAt(i)] < 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        Anagrams currentObject = new Anagrams();
+        System.out.println("Enter the strings to be checked for anagrams:");
+        Scanner scanner = new Scanner(System.in);
+        String input1 = scanner.nextLine();
+        String input2 = scanner.nextLine();
+        long startTime = System.nanoTime();
+        boolean result = currentObject.isAnagram(input1, input2);
+        long timeTaken = System.nanoTime() - startTime;
+        if(result)
+            System.out.println("Both strings are anagrams to each other.");
+        else
+            System.out.println("Both strings are not anagrams to each other.");
+        System.out.println("\nThe algorithm took " + timeTaken + " nanoseconds.");
+    }
+}
---
+++ src/DataStructures/UniqueChars.java
@@ -0,0 +1,42 @@
+package DataStructures;
+
+import java.util.Scanner;
+
+/**
+ * Created by honey on 6/3/2016.
+ */
+
+// Implement an algorithm to determine if a string has all unique characters
+
+public class UniqueChars
+{
+    boolean isUniqueChars(CharSequence input)
+    {
+        if(input.length()>256)
+            return false;
+        boolean[] char_flags = new boolean[256];
+        for (int i=0; i<input.length(); ++i)
+        {
+            if(char_flags[input.charAt(i)])
+                return false;
+            char_flags[input.charAt(i)] = true;
+        }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        UniqueChars currentObject = new UniqueChars();
+        System.out.println("Enter the string to be checked:");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        long startTime = System.nanoTime();
+        boolean result = currentObject.isUniqueChars(input);
+        long timeTaken = System.nanoTime() - startTime;
+        if(result)
+            System.out.println("Input contains all unique characters.");
+        else
+            System.out.println("Input does not contain all unique characters.");
+        System.out.println("\nThe algorithm took " + timeTaken + " nanoseconds.");
+    }
+}
