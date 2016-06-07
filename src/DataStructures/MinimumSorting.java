5f9631a3af6eba600c4043db01ab8e75bde76200
Honey Kakkar <honeykakkar09@gmail.com>
2016-06-06 23:58:50 -0400

    Adding basic problems related to array and linked list data structures

---
+++ src/DataStructures/MinimumSorting.java
@@ -0,0 +1,13 @@
+package DataStructures;
+
+/**
+ * Created by honey on 6/6/2016.
+ */
+/*
+Given an array of integers, write a method to find indices m and n such that if you
+sorted elements m through n, the entire array would be sorted. Minimize n - m (that
+is, find the smallest such sequence).
+*/
+
+public class MinimumSorting {
+}
