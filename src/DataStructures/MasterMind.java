cc99849f3d74813ac3643428014ce8b539b904c3
Honey Kakkar <honeykakkar09@gmail.com>
2016-06-07 23:50:14 -0400

    Adding basic problems related to array and linked list data structures

---
+++ src/DataStructures/MasterMind.java
@@ -0,0 +1,86 @@
+package DataStructures;
+
+import java.util.Scanner;
+
+/**
+ * Created by honey on 6/6/2016.
+ */
+
+/*
+* The computer has four slots, and each slot will contain a ball that is red (R), yellow
+(Y), green (G) or blue (B). For example, the computer might have RGGB (Slot# 1 is red,
+Slots #2 and #3 are green, Slot #4 is blue).
+You, the user, are trying to guess the solution. You might, for example, guess YRGB.
+When you guess the correct color for the correct slot, you get a "hit." If you guess a
+color that exists but is in the wrong slot, you get a "pseudo-hit." Note that a slot that
+is a hit can never count as a pseudo-hit.
+For example, if the actual solution is RGBY and you guess GGRR, you have one hit
+and one pseudo-hit.
+Write a method that, given a guess and a solution, returns the number of hits and
+pseudo-hits.*/
+
+
+public class MasterMind {
+
+    private static class Result{
+        int hits;
+        int pseudoHits;
+
+        Result(){
+            hits = 0;
+            pseudoHits = 0;
+        }
+
+        void incrementHits(){
+            ++hits;
+        }
+
+        void incrementPseudoHits(){
+            ++pseudoHits;
+        }
+    }
+
+    private int colorRepresentation(char color){
+        switch (color){
+            case 'R':return 0;
+            case 'G':return 1;
+            case 'Y':return 2;
+            case 'B':return 3;
+            default: return -1;
+        }
+    }
+
+    private void getResult(Result result, String guess, String solution) {
+        int[] occurences = new int[solution.length()];
+        for (int i = 0; i < guess.length(); ++i) {
+            if (guess.charAt(i) == solution.charAt(i))
+                result.incrementHits();
+            else
+                ++occurences[colorRepresentation(solution.charAt(i))];
+        }
+
+        for (int i = 0; i < guess.length(); ++i) {
+            char currentColor = guess.charAt(i);
+            if (currentColor != solution.charAt(i) && colorRepresentation(currentColor)>=0
+                    && occurences[colorRepresentation(currentColor)] > 0) {
+                result.incrementPseudoHits();
+                --occurences[colorRepresentation(currentColor)];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Result result = new Result();
+        MasterMind currentObject = new MasterMind();
+        System.out.println("The computer has four slots, and each slot will contain a ball that is red (R), yellow" +
+                "(Y), green (G) or blue (B).\nEnter your guess:");
+        Scanner scanner = new Scanner(System.in);
+        String guess = scanner.nextLine();
+        String solution = "BYYG";
+        long startTime = System.nanoTime();
+        currentObject.getResult(result, guess, solution);
+        long timeTaken = System.nanoTime() - startTime;
+        System.out.println("\nTotal Hits: " + result.hits + "\nTotal Pseudo-hits: " + result.pseudoHits);
+        System.out.println("\nThe algorithm took " + timeTaken +" nanoseconds.");
+    }
+}
--- src/DataStructures/MinimumSorting.java
+++ src/DataStructures/MinimumSorting.java
@@ -1,13 +1,96 @@
 package DataStructures;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by honey on 6/6/2016.
  */
-/*
-Given an array of integers, write a method to find indices m and n such that if you
+
+/*Given an array of integers, write a method to find indices m and n such that if you
 sorted elements m through n, the entire array would be sorted. Minimize n - m (that
 is, find the smallest such sequence).
 */
 
 public class MinimumSorting {
+
+    private static class Result{
+        int startIndex;
+        int endIndex;
+
+        Result(){
+           startIndex =0;
+            endIndex = 0;
+        }
+
+        void displayIndices(){
+            System.out.println("\nStart index is " + startIndex + "\nEnd index is " + endIndex);
+        }
+    }
+
+    private void findIndices(Result result, ArrayList<Integer> array) {
+        int tempStart = 0, tempEnd = array.size() - 1;
+        for (int i = 1; i < array.size(); ++i) {
+            if (array.get(i) >= array.get(i - 1))
+                ++tempStart;
+            else
+                break;
+        }
+        ++tempStart;
+
+        for (int i = array.size() - 1; i >= 0; --i) {
+            if (array.get(i) >= array.get(i - 1))
+                --tempEnd;
+            else
+                break;
+        }
+        --tempEnd;
+
+        int windowMin = Integer.MAX_VALUE, windowMax = Integer.MIN_VALUE;
+        for(int i=tempStart; i<=tempEnd; ++i){
+            if(array.get(i)>windowMax)
+                windowMax = array.get(i);
+            if(array.get(i)<windowMin)
+                windowMin = array.get(i);
+        }
+
+        for(int i= tempStart-1; i>=0; --i){
+            if(array.get(i)<=windowMin)
+            {
+                tempStart = i;
+                break;
+            }
+        }
+
+        for(int i= tempEnd+1; i<array.size(); ++i){
+            if(array.get(i)>=windowMax)
+            {
+                tempEnd = i;
+                break;
+            }
+        }
+
+        result.startIndex = tempStart;
+        result.endIndex = tempEnd - 1;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+        System.out.println("Enter elements of array below. Type \"end\" when done:");
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()) {
+            String input = scanner.next();
+            if (input.equalsIgnoreCase("end"))
+                break;
+            array.add(new Integer(input));
+        }
+        System.out.println("Showing elements in the array (insertion order):");
+        for (int current : array)
+            System.out.print(current + "\t");
+        Result result = new Result();
+        MinimumSorting currentObject = new MinimumSorting();
+        currentObject.findIndices(result, array);
+        result.displayIndices();
+    }
 }
