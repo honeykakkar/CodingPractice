5b54e31c775cdfcacd7b8acab26500c2b6d9179c
Honey Kakkar <honeykakkar09@gmail.com>
2016-07-13 23:43:20 -0400

    Adding few problems related to Trees data structure

---
+++ src/DataStructures/Trees/checkBST.java
@@ -0,0 +1,36 @@
+package DataStructures.Trees;
+
+/**
+ * Author: honey
+ * Project: CodingPractice
+ * Date created: 7/13/2016
+ */
+public class checkBST {
+
+    boolean isBST(BinaryTreeNode root, int min, int max){
+        if(root == null)
+            return true;
+        if(root.data > min && root.data < max){
+            if (isBST(root.left, min, root.data) && isBST(root.right, root.data, max))
+                return true;
+        }
+        return false;
+    }
+
+    boolean isBST(BinaryTreeNode root){
+        return isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public static void main(String[] args) {
+        BinaryTree BT = new BinaryTree();
+        BT.insert(9, 5, 20, 3, 7, 15, 22, 2);
+        checkBST currObject = new checkBST();
+        BT.displayTree();
+        long startTime = System.nanoTime();
+        boolean result = currObject.isBST(BT.getRoot());
+        long endTime = System.nanoTime();
+        System.out.println("The given binary tree is " + (result == true? "" : "not ") + "a binary search tree.");
+        System.out.println("\nThe algorithm took " + (endTime - startTime) + " nanoseconds.");
+
+    }
+}
